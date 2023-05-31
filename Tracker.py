374b189a4c9d5ae09d903142213a69bdee8b3fca
Abdelrahman Elhawary <abdelrahmanelhawary@Abdelrahmans-MacBook-Pro.local>
2023-05-31 20:08:13 +0300

    moved to oop

--- Tracker.py
+++ Tracker.py
@@ -1,96 +1,137 @@
 import zmq
 import time
-
-localhost = "10.152.107.121"
-PORT = "7878"
-ChangeCoordinates = "Coordinates"
-NewChatMessage = "Chat"
+import pickle
+from threading import Thread
 
 
-def ChangeCordinatesRequest(Player_ID, Game_ID, Direction):
-    Selected_Game_ID = 0  # Used to change the variable scope
-
-    # Looping to determine which GameID to edit upon
-    for Game in Data:
-        if (Game["GameID"] == Game_ID):
-            Selected_Game_ID = Data.index(Game)
-
-    # Looping to Determine Which Player to Edit Upon
-    # Data[Game][Players_Info][Player][Player Attributes]
-    for Player in Data[Selected_Game_ID]["Players_Info"]:
-        print(Player)
-        if (Player["ID"] == Player_ID):
-            Selected_Player = Data[Selected_Game_ID]["Players_Info"].index(
-                Player)
-    if (Direction == "UP"):
-        Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_Y"] += 1
-    elif (Direction == "RIGHT"):
-        Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_X"] += 1
-    elif (Direction == "LEFT"):
-        Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_X"] -= 1
-    Publish(ChangeCoordinates, "%s %s %s %s" % (Game_ID, Player_ID, Data[Selected_Game_ID]["Players_Info"]
-            [Selected_Player]["Position_X"], Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_Y"]))
-
-
-def Publish(Type, Message):
-    context = zmq.Context()
-    socket = context.socket(zmq.PUB)
-    socket.connect("tcp://%s:%s" % (localhost, PORT))
-    time.sleep(1)
-    for i in range(2):
-        if (Type == ChangeCoordinates):
-            print("G "+Message)
-            socket.send_string("G " + Message)
-        elif (Type == NewChatMessage):
-            socket.send_string("C " + Message)
-        time.sleep(1)
-
-
-def NewGame(Game_ID):
-    Data.append({"GameID": Game_ID, "Players_Info": []})
-
-
-def NewPlayer(Player_Name, Player_ID, Game_ID):
-    Selected_GameID = 0
-    for item in Data:
-        if (item["GameID"] == Game_ID):
-            Selected_GameID = Data.index(item)
-    print(Selected_GameID)
-    Data[Selected_GameID]["Players_Info"].append(
-        {"Name": Player_Name, "ID": Player_ID, "Position_X": 0, "Position_Y": 0})
-
-
-# =========================================
-# To be switched to Redis
-Data = [
-    {
-        "GameID": 1,
-        "Players_Info": [{
-            "Name": "Ziad",
-            "ID": 1,
-            "Position_X": 0,
-            "Position_Y": 0
-        },
+class Server:
+    localhost = "127.0.0.1"
+    pub_PORT = "7878" # publisher port
+    pull_PORT = "8787" # recieve port
+    rep_PORT = "12345" # server port
+    GetState = "GS"
+    ChangeCoordinates = "G"
+    NewChatMessage = "C"
+    Data = []
+    pub_socket = None
+    pull_socket = None
+    rep_socket = None
+    
+    
+    def __init__(self):
+        # =========================================
+        # To be switched to Redis
+        self.Data = [
             {
-            "Name": "OSOS",
-            "ID": 2
-        }
+                "GameID": 1,
+                "Players_Info": [{
+                    "ID": "ziad",
+                    "Position_X": 0,
+                    "Position_Y": 0
+                },
+                {
+                    "ID": "osos",
+                    "Position_X": 0,
+                    "Position_Y": 0
+                },
+                {
+                    "ID": "hawary",
+                    "Position_X": 0,
+                    "Position_Y": 0
+                }
+                ]
+            }
         ]
-    }
-]
-# To be switched to Redis
-# =========================================
+        context = zmq.Context()
+        self.pub_socket = context.socket(zmq.PUB)
+        self.pub_socket.bind("tcp://%s:%s" % (self.localhost, self.pub_PORT))
+        self.pull_socket = context.socket(zmq.PULL)
+        self.pull_socket.bind("tcp://%s:%s" % (self.localhost, self.pull_PORT))
+        self.rep_socket = context.socket(zmq.REP)
+        self.rep_socket.bind("tcp://%s:%s" % (self.localhost, self.rep_PORT))
+        # open server ports
+    # To be switched to Redis
+    # =========================================
+    # Message: GAME_ID TYPE PLAYER_ID DATA
+    # Message: 1 C hawary Alooooooo ya geda3an
+    # Message: 1 G ziad LEFT
 
+    def getGameIdx(self, game_id):
+        # Looping to determine which GameID to edit upon
+        Selected_Game_ID = -1
+        for Game in self.Data:
+            if (Game["GameID"] == game_id):
+                Selected_Game_ID = self.Data.index(Game)
+        return Selected_Game_ID
+    
+    
+    def ChangeCordinatesRequest(self, Player_ID, Game_ID, Direction):
+        Selected_Game_ID = self.getGameIdx(Game_ID)
 
-# print(Data)
-# NewGame(4)
-# print(Data)
-# NewPlayer("Ziad", 12, 4)
-# print(Data)
-# Data[0]["Data"].append({"Tester": 2})
-# print(Data[0]["Data"])
-# myList.append({"Name": "Hawary", "ID": 3})
-# print(myList)
-# NewPlayer("Hawary", 3, 0)
-# ChangeCordinatesRequest(12, 4, "UP")
-# print(Data)
+        # Looping to Determine Which Player to Edit Upon
+        # Data[Game][Players_Info][Player][Player Attributes]
+        Selected_Player = -1
+        for Player in self.Data[Selected_Game_ID]["Players_Info"]:
+            if (Player["ID"] == Player_ID):
+                Selected_Player = self.Data[Selected_Game_ID]["Players_Info"].index(
+                    Player)
+        if (Direction == "UP"):
+            self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_Y"] += 1
+        elif (Direction == "RIGHT"):
+            self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_X"] += 1
+        elif (Direction == "LEFT"):
+            self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_X"] -= 1
+        msg = "%s %s G %s %s" % (Game_ID, Player_ID, str(self.Data[Selected_Game_ID]["Players_Info"]
+                [Selected_Player]["Position_X"]), str(self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_Y"]))
+        print("sending: " + msg)
+        self.pub_socket.send_string(msg)
+    
+
+    def NewGame(self,Game_ID):
+        self.Data.append({"GameID": Game_ID, "Players_Info": []})
+    
+    
+    def NewPlayer(self, Player_Name, Player_ID, Game_ID):
+        Selected_GameID = 0
+        for item in Data:
+            if (item["GameID"] == Game_ID):
+                Selected_GameID = self.Data.index(item)
+        print(Selected_GameID)
+        self.Data[Selected_GameID]["Players_Info"].append(
+            {"Name": Player_Name, "ID": Player_ID, "Position_X": 0, "Position_Y": 0})
+
+    def listen_for_pipeline(self):
+        print("listenening for pull..")
+        while True:
+            msg = self.pull_socket.recv()
+            print("pipeline")
+            print(msg)
+            data = msg.decode().split()
+            if data[2] == self.NewChatMessage:
+                print("pushing back")
+                self.pub_socket.send(msg)
+            elif data[2] == self.ChangeCoordinates:
+                self.ChangeCordinatesRequest(data[1], data[0], data[3])
+
+    def listen_for_reply(self):
+        print("listening for reply..")
+        while True:
+            msg = self.rep_socket.recv().decode()
+            print("req/rep")
+            print(msg)
+            game_id, msg_type = msg.split(" ")
+            print(msg_type)
+            if msg_type == self.GetState:
+                print("sending back")
+                game_idx = self.getGameIdx(game_id)
+                self.rep_socket.send(pickle.dumps(self.Data[game_idx]))
+
+    def start(self):
+        t1 = Thread(target=self.listen_for_pipeline)
+        t2 = Thread(target=self.listen_for_reply)
+        t1.start()
+        t2.start()
+
+if __name__ == "__main__":
+    server = Server()
+    server.start()
