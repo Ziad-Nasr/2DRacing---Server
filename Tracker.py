6a952f761d1e9e4fea2c01fabfd0e6312ce88034
Abdelrahman Elhawary <abdelrahmanelhawary@Abdelrahmans-MacBook-Pro.local>
2023-06-17 01:00:15 +0300

    basic features done

--- Tracker.py
+++ Tracker.py
@@ -103,6 +103,15 @@ class Server:
             self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_X"] += 1
         elif (Direction == "LEFT"):
             self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_X"] -= 1
+
+        if self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_Y"] >= 100:
+            msg = "%s STOP" % (Game_ID)
+            self.pub_socket.send_string(msg)
+            self.pushToTracker(self.Data[Selected_Game_ID]["Other_Tracker"], msg)
+            self.Data[Selected_Game_ID]["Status"] = "Done"
+            res = requests.post("http://"+self.AUTH_SERVER+":5000/done", data={"game_id": Game_ID})
+            return
+
         msg = "%s %s G %s %s" % (Game_ID, Player_ID, str(self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_X"]), str(self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_Y"]))
         other_tracker_msg = "%s %s SSG %s %s" % (Game_ID, Player_ID, str(self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_X"]), str(self.Data[Selected_Game_ID]["Players_Info"][Selected_Player]["Position_Y"]))
         print("sending: " + msg)
@@ -112,13 +121,13 @@ class Server:
 
     def NewGame(self,Game_ID, other_tracker):
         print("Added Game %s With tracker %s" % (Game_ID, other_tracker))
-        self.Data.append({"GameID": Game_ID, "Other_Tracker": other_tracker, "Players_Info": []})
+        self.Data.append({"GameID": Game_ID, "Status":"Starting", "Other_Tracker": other_tracker, "Players_Info": []})
     
     
     def NewPlayer(self, Player_ID, Game_ID):
         Selected_GameID = self.getGameIdx(Game_ID)
-        x = len(self.Data[Selected_Game_ID]["Players_Info"]) % 5
-        y = len(self.Data[Selected_Game_ID]) // 5
+        x = (len(self.Data[Selected_GameID]["Players_Info"]) % 5) + 1
+        y = len(self.Data[Selected_GameID]) // 5
         self.Data[Selected_GameID]["Players_Info"].append({"ID": Player_ID, "Position_X": x, "Position_Y": y, "Ready": 0})
         msg = "%s %s J %s %s" % (Game_ID, Player_ID, x, y)
         print("PUBLISHING " + msg)
@@ -142,7 +151,10 @@ class Server:
                 sendStart = False
                 break
         if sendStart:
+            print("Sending Start Message")
+            self.Data[Selected_Game_ID]["Status"] = "Ongoing"
             self.pub_socket.send_string("%s START" % Game_ID)
+            self.pushToTracker(self.Data[Selected_Game_ID]["Other_Tracker"], "%s START" % Game_ID)
 
 
     def listen_for_pipeline(self):
@@ -154,31 +166,38 @@ class Server:
             data = msg.decode().split()
             game_id = data[0]
             player_id = data[1]
-            if data[2] == self.NewChatMessage:
-                print("pushing back")
-                self.pub_socket.send(msg)
-            elif data[2] == self.ChangeCoordinates:
-                self.ChangeCordinatesRequest(player_id, game_id, data[3])
-            elif data[2] == self.Join:
-                self.NewPlayer(player_id, game_id)
-            elif data[2] == self.Ready:
-                self.ReadyPlayer(player_id, game_id)
-            elif data[2] == "SG":
-                self.NewGame(game_id, data[3])
-            elif data[2] == self.SingleState+self.ChangeCoordinates: # game_id player_id SSG x y
-                x = data[3]
-                y = data[4]
+            if data[1] == "START":
                 game_idx = self.getGameIdx(game_id)
-                player_idx = self.getPlayerIdx(game_idx, player_id)
-                self.Data[game_idx]["Players_Info"][player_idx]["Position_X"] = x
-                self.Data[game_idx]["Players_Info"][player_idx]["Position_Y"] = y
-            elif data[2] == self.SingleState+self.Join: # game_id player_id SSJ
+                self.Data[game_idx]["Status"] = "Ongoing"
+            elif data[1] == "STOP":
                 game_idx = self.getGameIdx(game_id)
-                self.Data[game_idx]["Players_Info"].append({"ID": player_id, "Position_X": 0, "Position_Y": 0, "Ready": 0})
-            elif data[2] == self.SingleState+self.Ready: # game_id player_id SSR
-                game_idx = self.getGameIdx(game_id)
-                player_idx = self.getPlayerIdx(game_idx, player_id)
-                self.Data[game_idx]["Players_Info"][player_idx]["ready"] = 1
+                self.Data[game_idx]["Status"] = "Done"
+            if len(data) > 2:
+                if data[2] == self.NewChatMessage:
+                    print("pushing back")
+                    self.pub_socket.send(msg)
+                elif data[2] == self.ChangeCoordinates:
+                    self.ChangeCordinatesRequest(player_id, game_id, data[3])
+                elif data[2] == self.Join:
+                    self.NewPlayer(player_id, game_id)
+                elif data[2] == self.Ready:
+                    self.ReadyPlayer(player_id, game_id)
+                elif data[2] == "SG":
+                    self.NewGame(game_id, data[3])
+                elif data[2] == self.SingleState+self.ChangeCoordinates: # game_id player_id SSG x y
+                    x = data[3]
+                    y = data[4]
+                    game_idx = self.getGameIdx(game_id)
+                    player_idx = self.getPlayerIdx(game_idx, player_id)
+                    self.Data[game_idx]["Players_Info"][player_idx]["Position_X"] = int(x)
+                    self.Data[game_idx]["Players_Info"][player_idx]["Position_Y"] = int(y)
+                elif data[2] == self.SingleState+self.Join: # game_id player_id SSJ
+                    game_idx = self.getGameIdx(game_id)
+                    self.Data[game_idx]["Players_Info"].append({"ID": player_id, "Position_X": 0, "Position_Y": 0, "Ready": 0})
+                elif data[2] == self.SingleState+self.Ready: # game_id player_id SSR
+                    game_idx = self.getGameIdx(game_id)
+                    player_idx = self.getPlayerIdx(game_idx, player_id)
+                    self.Data[game_idx]["Players_Info"][player_idx]["Ready"] = 1
 
 
     def listen_for_reply(self):
@@ -228,9 +247,9 @@ class Server:
     def start(self):
         t1 = Thread(target=self.listen_for_pipeline)
         t2 = Thread(target=self.listen_for_reply)
+        self.getMyGames()
         t1.start()
         t2.start()
-        self.getMyGames()
 
 if __name__ == "__main__":
     server = Server()
